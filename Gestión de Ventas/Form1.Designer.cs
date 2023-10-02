a1030d03aa5db78ef92d2fcbdc774185a49572dd
Samuel Nissim <samuelnissimm@gmail.com>
2023-10-02 05:43:59 -0600

    Add project files.

---
+++ Gestión de Ventas/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Gestión_de_Ventas
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SideMenuPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.ClientsSubMenu = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.ClientsBtn = new System.Windows.Forms.Button();
+            this.ProvBtn = new System.Windows.Forms.Button();
+            this.OrdersSubMenu = new System.Windows.Forms.Panel();
+            this.IncompleteOrdersBtn = new System.Windows.Forms.Button();
+            this.CompleteOrdersBtn = new System.Windows.Forms.Button();
+            this.OrdersMenuBtn = new System.Windows.Forms.Button();
+            this.SideMenuBtn = new System.Windows.Forms.Button();
+            this.SideMenuPanel.SuspendLayout();
+            this.ClientsSubMenu.SuspendLayout();
+            this.OrdersSubMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SideMenuPanel
+            // 
+            this.SideMenuPanel.AutoScroll = true;
+            this.SideMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.SideMenuPanel.Controls.Add(this.panel3);
+            this.SideMenuPanel.Controls.Add(this.button10);
+            this.SideMenuPanel.Controls.Add(this.button9);
+            this.SideMenuPanel.Controls.Add(this.ClientsSubMenu);
+            this.SideMenuPanel.Controls.Add(this.ClientsBtn);
+            this.SideMenuPanel.Controls.Add(this.ProvBtn);
+            this.SideMenuPanel.Controls.Add(this.OrdersSubMenu);
+            this.SideMenuPanel.Controls.Add(this.OrdersMenuBtn);
+            this.SideMenuPanel.Controls.Add(this.SideMenuBtn);
+            this.SideMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SideMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.SideMenuPanel.Name = "SideMenuPanel";
+            this.SideMenuPanel.Size = new System.Drawing.Size(250, 853);
+            this.SideMenuPanel.TabIndex = 0;
+            this.SideMenuPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.SideMenuPanel_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(52)))), ((int)(((byte)(53)))));
+            this.panel3.BackgroundImage = global::Gestión_de_Ventas.Properties.Resources._306148787_512560377543920_5175169491060625538_n_png;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 694);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(250, 159);
+            this.panel3.TabIndex = 1;
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.LightGray;
+            this.button10.Location = new System.Drawing.Point(0, 491);
+            this.button10.Name = "button10";
+            this.button10.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button10.Size = new System.Drawing.Size(250, 52);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "Estadísticas";
+            this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.LightGray;
+            this.button9.Location = new System.Drawing.Point(0, 439);
+            this.button9.Name = "button9";
+            this.button9.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button9.Size = new System.Drawing.Size(250, 52);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "Historial";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // ClientsSubMenu
+            // 
+            this.ClientsSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.ClientsSubMenu.Controls.Add(this.button7);
+            this.ClientsSubMenu.Controls.Add(this.button8);
+            this.ClientsSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ClientsSubMenu.Location = new System.Drawing.Point(0, 335);
+            this.ClientsSubMenu.Name = "ClientsSubMenu";
+            this.ClientsSubMenu.Size = new System.Drawing.Size(250, 104);
+            this.ClientsSubMenu.TabIndex = 4;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.LightGray;
+            this.button7.Location = new System.Drawing.Point(0, 52);
+            this.button7.Name = "button7";
+            this.button7.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.button7.Size = new System.Drawing.Size(250, 52);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Black List";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.LightGray;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button8.Location = new System.Drawing.Point(0, 0);
+            this.button8.Name = "button8";
+            this.button8.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.button8.Size = new System.Drawing.Size(250, 52);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Todos";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // ClientsBtn
+            // 
+            this.ClientsBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ClientsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientsBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.ClientsBtn.Location = new System.Drawing.Point(0, 283);
+            this.ClientsBtn.Name = "ClientsBtn";
+            this.ClientsBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.ClientsBtn.Size = new System.Drawing.Size(250, 52);
+            this.ClientsBtn.TabIndex = 3;
+            this.ClientsBtn.Text = "Clientes";
+            this.ClientsBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ClientsBtn.UseVisualStyleBackColor = true;
+            this.ClientsBtn.Click += new System.EventHandler(this.ClientsBtn_Click);
+            // 
+            // ProvBtn
+            // 
+            this.ProvBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ProvBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ProvBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProvBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.ProvBtn.Location = new System.Drawing.Point(0, 231);
+            this.ProvBtn.Name = "ProvBtn";
+            this.ProvBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.ProvBtn.Size = new System.Drawing.Size(250, 52);
+            this.ProvBtn.TabIndex = 2;
+            this.ProvBtn.Text = "Proovedores";
+            this.ProvBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ProvBtn.UseVisualStyleBackColor = true;
+            // 
+            // OrdersSubMenu
+            // 
+            this.OrdersSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.OrdersSubMenu.Controls.Add(this.IncompleteOrdersBtn);
+            this.OrdersSubMenu.Controls.Add(this.CompleteOrdersBtn);
+            this.OrdersSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OrdersSubMenu.Location = new System.Drawing.Point(0, 127);
+            this.OrdersSubMenu.Name = "OrdersSubMenu";
+            this.OrdersSubMenu.Size = new System.Drawing.Size(250, 104);
+            this.OrdersSubMenu.TabIndex = 1;
+            // 
+            // IncompleteOrdersBtn
+            // 
+            this.IncompleteOrdersBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.IncompleteOrdersBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.IncompleteOrdersBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IncompleteOrdersBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.IncompleteOrdersBtn.Location = new System.Drawing.Point(0, 52);
+            this.IncompleteOrdersBtn.Name = "IncompleteOrdersBtn";
+            this.IncompleteOrdersBtn.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.IncompleteOrdersBtn.Size = new System.Drawing.Size(250, 52);
+            this.IncompleteOrdersBtn.TabIndex = 1;
+            this.IncompleteOrdersBtn.Text = "Incompletos";
+            this.IncompleteOrdersBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.IncompleteOrdersBtn.UseVisualStyleBackColor = true;
+            this.IncompleteOrdersBtn.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // CompleteOrdersBtn
+            // 
+            this.CompleteOrdersBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CompleteOrdersBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CompleteOrdersBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompleteOrdersBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.CompleteOrdersBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CompleteOrdersBtn.Location = new System.Drawing.Point(0, 0);
+            this.CompleteOrdersBtn.Name = "CompleteOrdersBtn";
+            this.CompleteOrdersBtn.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.CompleteOrdersBtn.Size = new System.Drawing.Size(250, 52);
+            this.CompleteOrdersBtn.TabIndex = 0;
+            this.CompleteOrdersBtn.Text = "Completos";
+            this.CompleteOrdersBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CompleteOrdersBtn.UseVisualStyleBackColor = true;
+            this.CompleteOrdersBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // OrdersMenuBtn
+            // 
+            this.OrdersMenuBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OrdersMenuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.OrdersMenuBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrdersMenuBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.OrdersMenuBtn.Location = new System.Drawing.Point(0, 75);
+            this.OrdersMenuBtn.Name = "OrdersMenuBtn";
+            this.OrdersMenuBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.OrdersMenuBtn.Size = new System.Drawing.Size(250, 52);
+            this.OrdersMenuBtn.TabIndex = 1;
+            this.OrdersMenuBtn.Text = "Pedidos";
+            this.OrdersMenuBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.OrdersMenuBtn.UseVisualStyleBackColor = true;
+            this.OrdersMenuBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SideMenuBtn
+            // 
+            this.SideMenuBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.SideMenuBtn.BackgroundImage = global::Gestión_de_Ventas.Properties.Resources.icons8_hamburger_button_50;
+            this.SideMenuBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.SideMenuBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SideMenuBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SideMenuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SideMenuBtn.ForeColor = System.Drawing.Color.White;
+            this.SideMenuBtn.Location = new System.Drawing.Point(0, 0);
+            this.SideMenuBtn.Name = "SideMenuBtn";
+            this.SideMenuBtn.Size = new System.Drawing.Size(250, 75);
+            this.SideMenuBtn.TabIndex = 1;
+            this.SideMenuBtn.UseVisualStyleBackColor = false;
+            this.SideMenuBtn.Click += new System.EventHandler(this.SideMenuBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 853);
+            this.Controls.Add(this.SideMenuPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "MainForm";
+            this.Text = "Gestión de Ventas";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.SideMenuPanel.ResumeLayout(false);
+            this.ClientsSubMenu.ResumeLayout(false);
+            this.OrdersSubMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SideMenuPanel;
+        private System.Windows.Forms.Button SideMenuBtn;
+        private System.Windows.Forms.Button OrdersMenuBtn;
+        private System.Windows.Forms.Panel OrdersSubMenu;
+        private System.Windows.Forms.Button CompleteOrdersBtn;
+        private System.Windows.Forms.Button IncompleteOrdersBtn;
+        private System.Windows.Forms.Button ProvBtn;
+        private System.Windows.Forms.Panel ClientsSubMenu;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button ClientsBtn;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Gestión de Ventas/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestión_de_Ventas
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (OrdersSubMenu.Visible) OrdersSubMenu.Visible = false;
+            else OrdersSubMenu.Visible = true;
+        }
+      
+
+        private void ClientsBtn_Click(object sender, EventArgs e)
+        {
+            if (ClientsSubMenu.Visible) ClientsSubMenu.Visible = false;
+            else ClientsSubMenu.Visible = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            OrdersSubMenu.Visible = false;
+            ClientsSubMenu.Visible = false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void SideMenuBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SideMenuPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
